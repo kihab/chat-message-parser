5ccbf46ec6d3be53e74cf0c6f5bbaa024dfcd818
Karim Ihab <karim.ihab@gmail.com>
2016-05-02 22:24:26 +0200

    Initial Commit

---
+++ AtlassianChatMessageParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AtlassianChatMessageParser
+//
+//  Created by Karim Ihab on 5/2/16.
+//  Copyright © 2016 Karim Ihab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
